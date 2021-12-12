0a4f73401ef7129667972267e55471da54169753
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-12-12 19:40:25 +0100

    remove

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/home/pebe/Atari/MadPascal:$PATH"
-mp kret.pas -o
-mads kret.a65 -x -i:/home/pebe/Atari/MadPascal/base -o:kret.xex
--- buildrun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH="/home/pebe/Atari/MadPascal:$PATH"
-mp kret.pas -o
-mads kret.a65 -x -t -l -i:/home/pebe/Atari/MadPascal/base -o:kret.xex
-wine c:/Altirra/Altirra kret.xex
